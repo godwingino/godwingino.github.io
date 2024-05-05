4d4e0e8b8abba3f76324d0c9074fa9d15f8d39b7
Godwin Gino <104904093+godwingino@users.noreply.github.com>
2024-05-05 17:54:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function myMenuFunction(){
 
 /* ----- TYPING EFFECT ----- */
  let typingEffect = new Typed(".typedText",{
-    strings : [" Cyber Security Enthusiast","UI Designer","Developer"],
+    strings : [" CyberSecurity Enthusiast","UI Designer","Developer"],
     loop : true,
     typeSpeed : 100, 
     backSpeed : 80,
