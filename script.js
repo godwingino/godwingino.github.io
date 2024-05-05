36b973aa0596850abd0a9e11e5405ba6e7f2f690
Godwin Gino <godwingino10@gmail.com>
2024-05-05 17:40:34 +0530

    update to cyber security

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function myMenuFunction(){
 
 /* ----- TYPING EFFECT ----- */
  let typingEffect = new Typed(".typedText",{
-    strings : ["UI/UX Designer","Creator","Developer"],
+    strings : [" Cyber Security Enthusiast","UI Designer","Developer"],
     loop : true,
     typeSpeed : 100, 
     backSpeed : 80,
@@ -116,3 +116,24 @@ function scrollActive() {
 }
 
 window.addEventListener('scroll', scrollActive)
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
